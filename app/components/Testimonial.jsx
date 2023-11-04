eadca616f8ae2f60b88c3518ce400e9646bbbd92
one_punch_man <ckim@banlaw.com>
2023-11-04 14:31:16 +1100

    nav bar

---
+++ app/components/Testimonial.jsx
@@ -0,0 +1,12 @@
+const Testimonial = () => {
+  return (
+    <section
+      className="section flex items-center bg-green-200"
+      id="testimonial"
+    >
+      Testimonial
+    </section>
+  );
+};
+
+export default Testimonial;
---
+++ app/components/Why.jsx
@@ -0,0 +1,9 @@
+const Why = () => {
+  return (
+    <section className="section flex items-center bg-blue-200" id="why">
+      Why
+    </section>
+  );
+};
+
+export default Why;
