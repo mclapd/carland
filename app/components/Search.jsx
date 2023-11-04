14ff82da3265fa046e0e9f35800b81a77f164184
one_punch_man <ckim@banlaw.com>
2023-11-05 10:37:13 +1100

    heor motion

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { SearchContext } from "../context/search";
 import LocationSelection from "./LocationSelection";
 import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 const Search = () => {
   const { searchActive } = useContext(SearchContext);
@@ -15,9 +16,21 @@ const Search = () => {
           : "bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
       } hidden xl:block w-full relative shadow-lg`}
     >
-      <div className="xl:h-full flex items-center px-6 xl:px-0">
+      <div className={`flex h-full ${searchActive && "container mx-auto"}`}>
         <LocationSelection />
         <DateSelection />
+        <HoursSelection />
+        <div className="xl:h-full flex items-center px-6 xl:px-0">
+          <button
+            className={`${
+              searchActive
+                ? "btn btn-sm btn-accent xl:w-[164px]"
+                : "btn btn-lg btn-accent xl:w-[184px]"
+            }`}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
